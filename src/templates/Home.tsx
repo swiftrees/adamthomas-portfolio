fe133d0f35e7326a8bdce851c279fd01d778a72e
swiftrees <swiftrees@outlook.com>
2024-02-27 03:26:25 +0000

    home layout

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -56,7 +56,6 @@ const HomeTemplate = () => {
 
   return (
     <div className="flex min-h-screen flex-col items-center p-4 lg:p-24">
-      
       <motion.div
         className="z-10 w-full items-center justify-between text-sm lg:flex flex-col"
         variants={responsiveAnimation}
@@ -71,9 +70,14 @@ const HomeTemplate = () => {
         <div className="flex flex-col lg:flex-row justify-between gap-x-16">
           <div className="flex flex-col w-full lg:w-1/3">
             <Education />
-            <Skills />
+            <div className="hidden md:flex flex-grow">
+              <Skills />
+            </div>
           </div>
           <WorkExperience />
+          <div className="block md:hidden">
+            <Skills />
+          </div>
         </div>
       </div>
     </div>
